0efc419abf0ada7ca616f092fafe15e9ebfa14b4
webit1723 <82347567+webit1723@users.noreply.github.com>
2023-04-08 11:25:38 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+    mustache = loadImage('https://i.postimg.cc/FRpkdj0m/Mustache-removebg-preview.png');
+}
+
+function setup() 
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();    
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results) {
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x-20;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(mustache, noseX, noseY, 50, 30);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
